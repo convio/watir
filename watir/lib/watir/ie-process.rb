9a2f33b08592706637e789301f7479748d39f1ed
Hugh McGowan <hugh_mcgowan@yahoo.com>
2011-04-29 09:48:28 -0500

    fix IE8 case where starting a new process does not work if a browser is already open

--- watir/lib/watir/ie-process.rb
+++ watir/lib/watir/ie-process.rb
@@ -3,14 +3,17 @@ require 'win32/process'
 module Watir
   class IE
     class Process
+
       def self.start
         program_files = ENV['ProgramFiles'] || "c:\\Program Files"
         startup_command = "#{program_files}\\Internet Explorer\\iexplore.exe"
+        startup_command << " -nomerge" if version_parts.first.to_i == 8
+        startup_command << " -extoff" if  ENV['IE_NOEXT'] == 'true'
         process_info = ::Process.create('app_name' => "#{startup_command} about:blank")
         process_id = process_info.process_id
         new process_id
       end
-      
+
       def initialize process_id
         @process_id = process_id
       end
@@ -33,14 +36,14 @@ module Watir
       end
       
       # Returns the process id for the specifed hWnd.
-      def self.process_id_from_hwnd hwnd
+      def process_id_from_hwnd hwnd
         pid_info = ' ' * 32
         Win32API.new('user32', 'GetWindowThreadProcessId', 'ip', 'i').
         call(hwnd, pid_info)
         process_id =  pid_info.unpack("L")[0]
       end
 
-      def self.version
+      def version
         @@ie_version ||= begin
           require 'win32/registry'
           ::Win32::Registry::HKEY_LOCAL_MACHINE.open("SOFTWARE\\Microsoft\\Internet Explorer") do |ie_key|
@@ -50,7 +53,7 @@ module Watir
         end
       end
 
-      def self.version_parts
+      def version_parts
         @@ie_version_parts ||= IE.version.split('.')
       end
 
