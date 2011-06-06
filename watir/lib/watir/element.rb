c09ffde404f811ccedceece26e6dd64ecb11124d
Hugh McGowan <hugh_mcgowan@yahoo.com>
2011-06-06 14:49:47 -0500

    add methods missed in the merge

--- watir/lib/watir/element.rb
+++ watir/lib/watir/element.rb
@@ -429,6 +429,20 @@ module Watir
         super
       end
     end
+
+    def nextsibling
+      assert_exists
+      result = Element.new(ole_object.nextSibling)
+      result.set_container self
+      result
+    end
+
+    def prevsibling
+      assert_exists
+      result = Element.new(ole_object.previousSibling)
+      result.set_container self
+      result
+    end
   end
 
   class ElementMapper # Still to be used
