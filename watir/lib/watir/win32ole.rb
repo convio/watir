242a44bd93653b474ca5a918c3dadd6710523cfd
Hugh McGowan <hugh_mcgowan@yahoo.com>
2012-02-13 14:15:24 -0600

    remove debug

--- watir/lib/watir/win32ole.rb
+++ watir/lib/watir/win32ole.rb
@@ -7,14 +7,8 @@ if RUBY_VERSION =~ /^1\.8/
   $LOAD_PATH.unshift  File.expand_path(File.join(File.dirname(__FILE__), '..', 'watir', 'win32ole', '1.8.7'))
 elsif RUBY_VERSION =~ /^1\.9/
   $LOAD_PATH.unshift  File.expand_path(File.join(File.dirname(__FILE__), '..', 'watir', 'win32ole', '1.9.3'))
-  puts 'here'
-  puts File.expand_path(File.join(File.dirname(__FILE__), '..', 'watir', 'win32ole', '1.9.3'))
 else
   # loading win32ole from stdlib
 end
-
-puts RUBY_VERSION
-puts File.expand_path(File.join(File.dirname(__FILE__), '..', 'watir', 'win32ole', '1.9.3'))
-puts require 'win32ole'
 
 WIN32OLE.codepage = WIN32OLE::CP_UTF8
