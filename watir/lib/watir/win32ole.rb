a42030e9d20436fa4bcb87c73a61dd18bea8be5b
Hugh McGowan <hugh_mcgowan@yahoo.com>
2012-02-13 16:19:56 -0600

    fix silly mistake

--- watir/lib/watir/win32ole.rb
+++ watir/lib/watir/win32ole.rb
@@ -3,7 +3,6 @@
 # Use our modified win32ole library
 if RUBY_VERSION =~ /^1\.8/
   $LOAD_PATH.unshift  File.expand_path(File.join(File.dirname(__FILE__), '..', 'watir', 'win32ole', '1.8.7'))
-  WIN32OLE.codepage = WIN32OLE::CP_UTF8
 elsif RUBY_VERSION =~ /^1\.9/
   $LOAD_PATH.unshift  File.expand_path(File.join(File.dirname(__FILE__), '..', 'watir', 'win32ole', '1.9.3'))
 else
