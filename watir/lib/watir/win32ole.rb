0caabe99a9f7a2c381d8df014a90ac770009adce
Hugh McGowan <hugh_mcgowan@yahoo.com>
2012-02-13 14:16:42 -0600

    remove more debug. argh

--- watir/lib/watir/win32ole.rb
+++ watir/lib/watir/win32ole.rb
@@ -1,8 +1,6 @@
 # load the correct version of win32ole
 
 # Use our modified win32ole library
-puts 'HERHEHREHRHERHERHER'
-
 if RUBY_VERSION =~ /^1\.8/
   $LOAD_PATH.unshift  File.expand_path(File.join(File.dirname(__FILE__), '..', 'watir', 'win32ole', '1.8.7'))
 elsif RUBY_VERSION =~ /^1\.9/
